1906c1e6795ea0c4a9e4899783417866dfdf971c
EisaScience <eisamahyari@gmail.com>
2019-03-08 12:11:55 -0800

    Documentation

--- rbin/CustomFXs_Seurat_pipeline.R
+++ rbin/CustomFXs_Seurat_pipeline.R
@@ -4,14 +4,14 @@
 
 
 
-
 CTL_Immune_GeneList <- function(QuickGO.path="./data/QuickGO"){
   
   SGS.LS <- list()
   SGS.LS$HighlyActivated <- c("TNFRSF9", "NFKBID", "CCL4", "CCL4L2", "IRF8", "IxNG", "CD83", "CD82", "PLEK", "RGCC") #"ENSMMUG00000013779"
   SGS.LS$LessActivated <- c("LTB", "IL7R", "PTPRCAP", "GZMK")
   SGS.LS$Pheno1 <- c("MT1M", "MGEA5")
-  SGS.LS$CD8Canonical <- c("CD8A", "CD8B", "CD3G", "CD3D", "CD3E") 
+  SGS.LS$TCellCanonical <- c("CD3G", "CD3D", "CD3E") 
+  SGS.LS$CD8Canonical <- c("CD8A", "CD8B") 
   SGS.LS$BCellCanonical <- c("CD19", "MS4A1", "CD79A") 
   SGS.LS$CD4Canonical <- c("CD4", "IL7R")
   SGS.LS$NKCanonical <- c("GZMK", "LYAR", "NKG7", "GZMA", "GNLY", "FGFBP2", "FCGR3A", "CCL4", "GZMH")
